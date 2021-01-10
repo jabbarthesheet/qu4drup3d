998908e2230ec265aceeb271eb2e00906acafc4f
jabbarthesheet <pierre.pardessus@gmail.com>
2021-01-10 21:09:08 +0100

    update 100121

--- src/pages/accueil/accueil.ts
+++ src/pages/accueil/accueil.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, NgModule } from '@angular/core';
-import { App, MenuController, NavController, NavParams, PickerController, PickerOptions, PopoverController, ToastController } from 'ionic-angular';
+import { AlertController, App, MenuController, NavController, NavParams, PickerController, PickerOptions, PopoverController, ToastController } from 'ionic-angular';
 import { ModalController, ViewController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { Nav } from 'ionic-angular';
@@ -19,6 +19,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { AntibioprophylaxiePage } from '../antibioprophylaxie/antibioprophylaxie';
 import { ScoresPage } from '../scores/scores';
 import { ProtocolesPage } from '../protocoles/protocoles';
+import { text } from '@angular/core/src/render3/instructions';
 
 
 
@@ -79,14 +80,13 @@ public time = "00:00:00";
     public ToastController: ToastController, 
     public popoverController: PopoverController,
     public menu: MenuController,
+    public alertController : AlertController,
   ) {}
     
   menuToggle()
   {
     this.menu.open();
   };
-
-
   /*** ----------------------------------------------------------TIMER----------------------------------------------------  */ 
 
 start() {
@@ -430,7 +430,25 @@ start() {
               prefix: "m.",
               options: [
                 { text: '0', value: 0 },
+                { text: '0,10', value: 0.1 },
+                { text: '0,20', value: 0.2 },
+                { text: '0,30', value: 0.3 },
+                { text: '0,40', value: 0.4 },
+                { text: '0,50', value: 0.5 },
+                { text: '0,60', value: 0.6 },
+                { text: '0,70', value: 0.7 },
+                { text: '0,80', value: 0.8 },
+                { text: '0,90', value: 0.9 },
                 { text: '1', value: 1 },
+                { text: '1,10', value: 1.1 },
+                { text: '1,20', value: 1.2 },
+                { text: '1,30', value: 1.3 },
+                { text: '1,40', value: 1.4 },
+                { text: '1,50', value: 1.5 },
+                { text: '1,60', value: 1.6 },
+                { text: '1,70', value: 1.7 },
+                { text: '1,80', value: 1.8 },
+                { text: '1,90', value: 1.9 },
                 { text: '2', value: 2 },
               ]
             },
@@ -447,99 +465,6 @@ start() {
                   { text: '7', value: 7 },
                   { text: '8', value: 8 },
                   { text: '9', value: 9 },
-                  { text: '10', value: 10 },
-                  { text: '11', value: 11 },
-                  { text: '12', value: 12 },
-                  { text: '13', value: 13 },
-                  { text: '14', value: 14 },
-                  { text: '15', value: 15 },
-                  { text: '16', value: 16 },
-                  { text: '17', value: 17 },
-                  { text: '18', value: 18 },
-                  { text: '19', value: 19 },
-                  { text: '20', value: 20 },
-                  { text: '21', value: 21 },
-                  { text: '22', value: 22 },
-                  { text: '23', value: 23 },
-                  { text: '24', value: 24 },
-                  { text: '25', value: 25 },
-                  { text: '26', value: 26 },
-                  { text: '27', value: 27 },
-                  { text: '28', value: 28 },
-                  { text: '29', value: 29 },
-                  { text: '30', value: 30 },
-                  { text: '31', value: 31 },
-                  { text: '32', value: 32 },
-                  { text: '33', value: 33 },
-                  { text: '34', value: 34 },
-                  { text: '35', value: 35 },
-                  { text: '36', value: 36 },
-                  { text: '37', value: 37 },
-                  { text: '38', value: 38 },
-                  { text: '39', value: 39 },
-                  { text: '40', value: 40 },
-                  { text: '41', value: 41 },
-                  { text: '42', value: 42 },
-                  { text: '43', value: 43 },
-                  { text: '44', value: 44 },
-                  { text: '45', value: 45 },
-                  { text: '46', value: 46 },
-                  { text: '47', value: 47 },
-                  { text: '48', value: 48 },
-                  { text: '49', value: 49 },
-                  { text: '50', value: 50 },
-                  { text: '51', value: 51 },
-                  { text: '52', value: 52 },
-                  { text: '53', value: 53 },
-                  { text: '54', value: 54 },
-                  { text: '55', value: 55 },
-                  { text: '56', value: 56 },
-                  { text: '57', value: 57 },
-                  { text: '58', value: 58 },
-                  { text: '59', value: 59 },
-                  { text: '60', value: 60 },
-                  { text: '61', value: 61 },
-                  { text: '62', value: 62 },
-                  { text: '63', value: 63 },
-                  { text: '64', value: 64 },
-                  { text: '65', value: 65 },
-                  { text: '66', value: 66 },
-                  { text: '67', value: 67 },
-                  { text: '68', value: 68 },
-                  { text: '69', value: 69 },
-                  { text: '70', value: 70 },
-                  { text: '71', value: 71 },
-                  { text: '72', value: 72 },
-                  { text: '73', value: 73 },
-                  { text: '74', value: 74 },
-                  { text: '75', value: 75 },
-                  { text: '76', value: 76 },
-                  { text: '77', value: 77 },
-                  { text: '78', value: 78 },
-                  { text: '79', value: 79 },
-                  { text: '80', value: 80 },
-                  { text: '81', value: 81 },
-                  { text: '82', value: 82 },
-                  { text: '83', value: 83 },
-                  { text: '84', value: 84 },
-                  { text: '85', value: 85 },
-                  { text: '86', value: 86 },
-                  { text: '87', value: 87 },
-                  { text: '88', value: 88 },
-                  { text: '89', value: 89 },
-                  { text: '90', value: 90 },
-                  { text: '91', value: 91 },
-                  { text: '92', value: 92 },
-                  { text: '93', value: 93 },
-                  { text: '94', value: 94 },
-                  { text: '95', value: 95 },
-                  { text: '96', value: 96 },
-                  { text: '97', value: 97 },
-                  { text: '98', value: 98 },
-                  { text: '99', value: 99 },
-                  { text: '100', value: 100 },
-                  
-  
                 ]
               },
             ]
@@ -548,6 +473,9 @@ start() {
       let picker = await this.pickerController.create(options);
       picker.present();
     }
+
+/* ----------------------------RECUPERATION DES DONNEES ANTERIEURES LE CAS ECHEANT---------------------------------------------------------------------------- */
+
 
     ionViewWillEnter() {
       let promiseList: Promise<any>[] = [];
@@ -584,6 +512,10 @@ start() {
     this.storage.set("sexeMF", this.sexeMF);
     console.log("sexe enregistré : ", this.sexeMF);
   };
+
+/* ----------------------------BOUTONS DE NAVIGATION---------------------------------------------------------------------------- */
+
+
 
     displayInduction(): void {
       this.navCtrl.push(PlateauInductionPage);
@@ -642,21 +574,29 @@ start() {
     };
 
 
-      
+/* ----------------------------BOUTON DAIDE---------------------------------------------------------------------------- */
+
     async help() {
-      const toastHelp = await this.ToastController.create({
-        message: "Pour utiliser QuadruPed, renseignez un âge et un poids pour le patient en cliquant sur les boutons. Cliquez ensuite sur les différents boutons pour obtenir des suggestions de posologie et de stratégies. Renseignez la taille et le sexe du patient pour utiliser les données de biométrie.",
-        position: 'Bottom',
-        showCloseButton: true,
-        closeButtonText: "OK.",
-        duration:5000, 
-        });
-      toastHelp.present();
-    };
+      const alert = await this.alertController.create({
+        cssClass: 'alerte',
+        title: 'Utiliser QuadruPed',
+        message: 'Pour utiliser QuadruPed, renseignez un âge et un poids pour le patient en appuyant sur les boutons correspondant. Naviguez ensuite entre les différents onglets pour obtenir des suggestions de posologie et de stratégies. Renseignez la taille et le sexe du patient pour utiliser les données de biométrie.',
+        buttons: [
+          {
+            text: "J'ai compris !",
+            handler: () =>  {
+              return; 
+            } } 
+        ],
+      });
+      await alert.present();
+      };
+  
+/* ----------------------------BOUTON EFFACER---------------------------------------------------------------------------- */
 
 
     effacer(){
-      this.storage.clear();
+      this.storage.clear(); /* ATTTENTION EFFACE TOUT TOTALEMENT TOUT */
       this.AgeNum=0;
       this.ageLecture=0;
       this.PoidsNum=0;
